f2a1bb96b9ceab2f8ef4312ea8a5636a0e93e199
Lance Addison <lance@lanceaddison.onmicrosoft.com>
2021-08-20 00:12:33 -0700

    Changed the way it randomly selects the messages

--- packingMessages.js
+++ packingMessages.js
@@ -1,8 +1,48 @@
-// These are all of the messages available inside an array.
-const messages = ['Time to pack up pots and pans!', "Let's pack up towls now.", "Let's do bowles and plates too.", "Now for the tv and games.", "Finish packing the bathroom.", 
-"Bedrooms need to be packed now.", "Furniture needs to be moved out!", "All cleaning supplies need to be boxed up!", "The pantry and food need to be done to!", "Lets get the garage done to.", 
-"Lets move all the boxed out to the garage.", "Time to load it all up and move!"];
+// messages are in an object and stored in arrays
+const packing = {
+    items: ['towels', 'tv', 'beds', 'toothbrush', 'dresser', 'movies', 'bowls', 'forks', 'food'], 
+    boxes: ['big', 'small', 'medium', 'extra small', 'extra large'],
+    leave: ['Monday', 'Tuesday', 'Wednesday', "Thursday", "Friday", "Saturday", "Sunday"],
+    punctuation: ['.', '!'],
+};
+ 
+// generates a random number
+function generateRandomNumber (num) {
+    return Math.floor(Math.random() * num);
+}
 
-let randomMessage = messages[Math.floor(Math.random() * messages.length)];
+// an empty array for the final message
+finalMessage = [];
 
-console.log(randomMessage);
+// random selection for punctuation
+let punct = packing.punctuation[Math.floor(Math.random() * packing.punctuation.length)];
+
+// generating a random number to select an array in the object
+for (msg in packing) {
+    randMessage = generateRandomNumber(packing[msg].length);
+    // selecting a message randomly from the array
+    switch (msg) {
+        case 'items':
+            punct = packing.punctuation[Math.floor(Math.random() * packing.punctuation.length)];
+            finalMessage.push(`You need to pack the ${packing[msg][randMessage]}${punct}`);
+            break;
+        case 'boxes':
+            punct = packing.punctuation[Math.floor(Math.random() * packing.punctuation.length)];
+            finalMessage.push(`We need a ${packing[msg][randMessage]}${punct}`);
+            break;
+        case 'leave':
+            punct = packing.punctuation[Math.floor(Math.random() * packing.punctuation.length)];
+            finalMessage.push(`We are leaving on ${packing[msg][randMessage]}${punct}`);
+            break;
+    }   
+    
+}
+
+// formating the final message array
+const packingFormated = pack => {
+    const format = pack.join('\n');
+    return '\n' + format + '\n';
+}
+
+// logging the final message to the console
+console.log(packingFormated(finalMessage));
